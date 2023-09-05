b6b2099360b07aefa53a970ff937ea3d5984fd94
gerasimgh <gerasgharibyan@mail.ru>
2023-09-05 14:27:01 +0400

    Initial commit

---
+++ src/main/java/com/example/buysell/models/Product.java
@@ -0,0 +1,46 @@
+package com.example.buysell.models;
+
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String title;
+    private String description;
+    private int price;
+    private String city;
+    private String author;
+
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY,
+    mappedBy = "product")
+    private List<Image> images = new ArrayList<>();
+    private Long previewImageId;
+    private LocalDateTime dateOfCreated;
+    @PrePersist
+    private void init(){
+        dateOfCreated=LocalDateTime.now();
+    }
+
+    public void addImageToProduct(Image image){
+        image.setProduct(this);
+        images.add(image);
+    }
+
+
+}
